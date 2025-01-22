d79eaba2da815a2186ab631afa3a5366fb4a47d4
jenconn <ye1wyy@gmail.com>
2025-01-22 13:24:58 +0700

    main: updated client model

--- internal/model/model_client.go
+++ internal/model/model_client.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 type Client struct {
-	Id               int64  `json:"id,omitempty" bson:"id,omitempty"`
-	ClientName       string `json:"client_name" bson:"client_name"`
-	ClientSurname    string `json:"client_surname" bson:"client_surname"`
-	Birthday         string `json:"birthday" bson:"birthday"`
-	Gender           string `json:"gender" bson:"gender"`
-	RegistrationDate string `json:"registration_date" bson:"registration_date"`
-	AddressId        int64  `json:"address_id" bson:"address_id"`
+	Id               string    `json:"_id,omitempty" bson:"_id,omitempty"`
+	ClientName       string    `json:"client_name" bson:"client_name"`
+	ClientSurname    string    `json:"client_surname" bson:"client_surname"`
+	Birthday         time.Time `json:"birthday" bson:"birthday"`
+	Gender           string    `json:"gender" bson:"gender"`
+	RegistrationDate time.Time `json:"registration_date" bson:"registration_date"`
+	AddressId        string    `json:"address_id" bson:"address_id"`
 }
