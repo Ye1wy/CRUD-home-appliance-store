6cd0c125cb7f81b4feb058c9e2be2862d8182828
jenconn <ye1wyy@gmail.com>
2025-01-22 17:24:35 +0700

    main: created mappers to DTO and to Model.Client

---
+++ internal/mapper/clientMapper.go
@@ -0,0 +1,60 @@
+package mapper
+
+import (
+	"CRUD-HOME-APPLIANCE-STORE/internal/dto"
+	"CRUD-HOME-APPLIANCE-STORE/internal/model"
+	"time"
+)
+
+const dateFormat = "2006-01-02"
+
+func ToClientDTO(client *model.Client) *dto.ClientDTO {
+	return &dto.ClientDTO{
+		Name:      client.ClientName,
+		Surname:   client.ClientSurname,
+		Birthday:  client.Birthday.Format(dateFormat),
+		Gender:    client.Gender,
+		AddressID: client.AddressId,
+	}
+}
+
+func ToClientModel(dto *dto.ClientDTO) (*model.Client, error) {
+	dtoBirthday, err := time.Parse(dateFormat, dto.Birthday)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Client{
+		ClientName:    dto.Name,
+		ClientSurname: dto.Surname,
+		Birthday:      dtoBirthday,
+		Gender:        dto.Gender,
+		AddressId:     dto.AddressID,
+	}, nil
+}
+
+func ToClientDTOs(clients []model.Client) []dto.ClientDTO {
+	dtos := make([]dto.ClientDTO, len(clients))
+
+	for i, client := range clients {
+		dtos[i] = *ToClientDTO(&client)
+	}
+
+	return dtos
+}
+
+func ToClientModels(dtos []dto.ClientDTO) ([]model.Client, error) {
+	models := make([]model.Client, len(dtos))
+
+	for i, dto := range dtos {
+		client, err := ToClientModel(&dto)
+		if err != nil {
+			return nil, err
+		}
+
+		models[i] = *client
+	}
+
+	return models, nil
+}
