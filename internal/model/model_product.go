e5aba84ab230b25f75902066aa4e59962fab3b8a
jenconn <ye1wyy@gmail.com>
2025-02-19 18:25:57 +0700

    main: Update model product

--- internal/model/model_product.go
+++ internal/model/model_product.go
@@ -1,12 +1,14 @@
 package model
 
+import "time"
+
 type Product struct {
-	Id             int64   `json:"id,omitempty" bson:"id,omitempty"`
-	Name           string  `json:"name" bson:"name"`
-	Category       string  `json:"category" bson:"category"`
-	Price          float32 `json:"price" bson:"price"`
-	AvailableStock int64   `json:"available_stock" bson:"available_stock"`
-	LastUpdateDate string  `json:"last_update_date" bson:"last_update_date"`
-	SupplierId     int64   `json:"supplier_id" bson:"supplier_id"`
-	ImageId        string  `json:"image_id" bson:"image_id"`
+	Id             string    `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name           string    `json:"name" bson:"name"`
+	Category       string    `json:"category" bson:"category"`
+	Price          float32   `json:"price" bson:"price"`
+	AvailableStock int64     `json:"available_stock" bson:"available_stock"`
+	LastUpdateDate time.Time `json:"last_update_date" bson:"last_update_date"`
+	SupplierId     string    `json:"supplier_id" bson:"supplier_id"`
+	ImageId        string    `json:"image_id" bson:"image_id"`
 }
