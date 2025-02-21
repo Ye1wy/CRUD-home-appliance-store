7801b9ea413a73f21fead76e0c118d40c4881f4b
jenconn <ye1wyy@gmail.com>
2025-02-21 17:13:19 +0700

    main: created product mapper (model to dto and back)

---
+++ internal/mapper/productMapper.go
@@ -0,0 +1,49 @@
+package mapper
+
+import (
+	"CRUD-HOME-APPLIANCE-STORE/internal/dto"
+	"CRUD-HOME-APPLIANCE-STORE/internal/model"
+)
+
+func ToProductDTO(product *model.Product) *dto.ProductDTO {
+	return &dto.ProductDTO{
+		Name:           product.Name,
+		Category:       product.Category,
+		Price:          product.Price,
+		AvailableStock: product.AvailableStock,
+		SupplierId:     product.SupplierId,
+		ImageId:        product.ImageId,
+	}
+}
+
+func ToProductModel(dto *dto.ProductDTO) *model.Product {
+	return &model.Product{
+		Name:           dto.Name,
+		Category:       dto.Category,
+		Price:          dto.Price,
+		AvailableStock: dto.AvailableStock,
+		SupplierId:     dto.SupplierId,
+		ImageId:        dto.ImageId,
+	}
+}
+
+func ToProductDTOs(products []model.Product) []dto.ProductDTO {
+	dtos := make([]dto.ProductDTO, len(products))
+
+	for i, product := range products {
+		dtos[i] = *ToProductDTO(&product)
+	}
+
+	return dtos
+}
+
+func ToProductModels(dtos []dto.ProductDTO) []model.Product {
+	models := make([]model.Product, len(dtos))
+
+	for i, dto := range dtos {
+		product := ToProductModel(&dto)
+		models[i] = *product
+	}
+
+	return models
+}
