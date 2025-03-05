2e4f845a1789d8d3e9a0d4120cfa9288dcb428f7
jenconn <ye1wyy@gmail.com>
2025-03-05 21:13:45 +0700

    main: added log messages and handle run errors, change main.go direcory

--- cmd/main.go
+++ cmd/main.go
@@ -13,22 +13,29 @@ import (
 
 func main() {
 	cfg := config.MustLoad()
+	cfg.PrintInfo()
 	log := logger.NewLogger(cfg.Env)
+	log.Info("Logger is created")
 	storage, err := mongodb.Connect(cfg.MongoURL, log)
+	log.Info("Connection is established")
 	if err != nil {
-		log.Error("[Error] Error in connetion to mongoDB: ", logger.Err(err))
+		log.Error("Error in connetion to mongoDB: ", logger.Err(err))
 		os.Exit(1)
 	}
 
-	log.Info("[INFO] Server started")
-
 	clientRepo := repositories.NewMongoClientRepository(storage.Database)
+	log.Info("MongoDB client repository is created")
 	clientService := services.NewClientService(clientRepo)
+	log.Info("MongoDB client service is created")
 	clientController := controllers.NewClientsController(clientService, log)
+	log.Info("MongoDB client controller is created")
 
 	productRepo := repositories.NewMongoProductsRepository(storage.Database)
+	log.Info("MongoDB product repository is created")
 	productService := services.NewProductService(productRepo)
+	log.Info("MongoDB product service is created")
 	productController := controllers.NewProductController(productService, log)
+	log.Info("MongoDB product controller is created")
 
 	routerConfig := routes.RouterConfig{
 		ClientController:  clientController,
@@ -36,5 +43,10 @@ func main() {
 	}
 
 	router := routes.NewRouter(routerConfig)
-	router.Run(":8080")
+	log.Info("The paths are laid")
+
+	log.Info("Server started")
+	if err := router.Run(":" + cfg.Port); err != nil {
+		log.Error("Error in start server: ", logger.Err(err))
+	}
 }
