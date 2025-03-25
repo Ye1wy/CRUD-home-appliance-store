b62c35315f31f925b0d44a185483e1c9d0fe0526
jenconn <ye1wyy@gmail.com>
2025-03-25 17:57:43 +0700

    update: update client controller due to the base controller

--- internal/controllers/clientController.go
+++ internal/controllers/clientController.go
@@ -4,7 +4,6 @@ import (
 	"CRUD-HOME-APPLIANCE-STORE/internal/dto"
 	"CRUD-HOME-APPLIANCE-STORE/internal/logger"
 	"CRUD-HOME-APPLIANCE-STORE/internal/services"
-	"log/slog"
 	"net/http"
 	"strconv"
 
@@ -12,14 +11,15 @@ import (
 )
 
 type ClientsController struct {
+	*BaseController
 	service services.ClientsService
-	logger  *slog.Logger
 }
 
-func NewClientsController(service services.ClientsService, log *slog.Logger) *ClientsController {
+func NewClientsController(service services.ClientsService, logger *logger.Logger) *ClientsController {
+	controller := NewBaseContorller(logger)
 	return &ClientsController{
-		service: service,
-		logger:  log,
+		BaseController: controller,
+		service:        service,
 	}
 }
 
@@ -30,23 +30,22 @@ func NewClientsController(service services.ClientsService, log *slog.Logger) *Cl
 // 500
 func (ctrl *ClientsController) AddClient(c *gin.Context) {
 	op := "controllers.client.addClient"
-
 	var clientDTO dto.ClientDTO
 	if err := c.ShouldBindJSON(&clientDTO); err != nil {
 		ctrl.logger.Error("Failed to bind JSON for AddClient", logger.Err(err), "op", op)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		ctrl.responce(c, http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
 
 	client, err := ctrl.service.AddClient(c.Request.Context(), &clientDTO)
 	if err != nil {
 		ctrl.logger.Error("Failed to add client: ", logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add client"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Failed to add client"})
 		return
 	}
 
 	ctrl.logger.Info("Client added successfully", "clientID", client.Id, "op", op)
-	c.JSON(http.StatusCreated, client)
+	ctrl.responce(c, http.StatusCreated, client)
 }
 
 // Get /api/v1/clients
@@ -56,30 +55,29 @@ func (ctrl *ClientsController) AddClient(c *gin.Context) {
 // 500:
 func (ctrl *ClientsController) GetAllClients(c *gin.Context) {
 	op := "controllers.client.getAllClients"
-
 	limit, err := strconv.Atoi(c.DefaultQuery("limit", defaultLimit))
 	if err != nil {
 		ctrl.logger.Error("Invalid limit parameter", logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid limit parameter"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Server is busy"})
 		return
 	}
 
 	offset, err := strconv.Atoi(c.DefaultQuery("offset", defaultOffset))
 	if err != nil {
 		ctrl.logger.Error("Invalid offset parameter", logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid offset parameter"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Server is busy"})
 		return
 	}
 
 	client, err := ctrl.service.GetAllClients(c.Request.Context(), limit, offset)
 	if err != nil {
 		ctrl.logger.Error("Failed to retrieve clients", logger.Err(err), "op", op)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to retrieve client"})
+		ctrl.responce(c, http.StatusBadRequest, gin.H{"error": "Failed to retrieve client"})
 		return
 	}
 
 	ctrl.logger.Info("Retrieved all clients", "limit", limit, "offset", offset, "op", op)
-	c.JSON(http.StatusOK, client)
+	ctrl.responce(c, http.StatusOK, client)
 }
 
 // Get /api/v1/clients?name=&surname=
@@ -89,26 +87,24 @@ func (ctrl *ClientsController) GetAllClients(c *gin.Context) {
 // 404
 func (ctrl *ClientsController) GetClientByNameAndSurname(c *gin.Context) {
 	op := "controllers.client.getClientByNameAndSurname"
-
 	name := c.Query("name")
 	surname := c.Query("surname")
-
 	clientDTO, err := ctrl.service.GetClientByNameAndSurname(c.Request.Context(), name, surname)
 
 	if clientDTO == nil && err == nil {
 		ctrl.logger.Warn("Client not found", "op", op)
-		c.JSON(http.StatusNotFound, gin.H{"error": "Client not found"})
+		ctrl.responce(c, http.StatusNotFound, gin.H{"error": "Client not found"})
 		return
 	}
 
 	if err != nil {
 		ctrl.logger.Error("Error while searching client ", logger.Err(err), "op", op)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Name and surname cannot be empty!"})
+		ctrl.responce(c, http.StatusBadRequest, gin.H{"error": "Name and surname cannot be empty!"})
 		return
 	}
 
 	ctrl.logger.Info("Client retrieved successfully", "name", name, "surname", surname, "op", op)
-	c.JSON(http.StatusOK, clientDTO)
+	ctrl.responce(c, http.StatusOK, clientDTO)
 }
 
 // Patch /api/v1/clients/:id/address
@@ -118,25 +114,23 @@ func (ctrl *ClientsController) GetClientByNameAndSurname(c *gin.Context) {
 // 500
 func (ctrl *ClientsController) ChangeAddressParameter(c *gin.Context) {
 	op := "controllers.client.changeAddressParameter"
-
 	id := c.Param("id")
-
 	var clientDTO dto.UpdateClientAddressDTO
 
 	if err := c.ShouldBindJSON(&clientDTO); err != nil {
 		ctrl.logger.Error("Failed to bind JSON for ChangeAddressIdParameter", logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid request payload"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Invalid request payload"})
 		return
 	}
 
 	if err := ctrl.service.ChangeAddressParameter(c.Request.Context(), id, clientDTO.AddressID); err != nil {
 		ctrl.logger.Error("Failed to update address ID", "clientID", id, logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update address ID"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Failed to update address ID"})
 		return
 	}
 
 	ctrl.logger.Info("Address ID updated successfully", "clientID", id, "newAddressID", clientDTO.AddressID, "op", op)
-	c.JSON(http.StatusOK, gin.H{"status": "Address ID updated"})
+	ctrl.responce(c, http.StatusOK, gin.H{"status": "Address ID updated"})
 }
 
 // Delete /api/v1/clients/:id
@@ -151,7 +145,7 @@ func (ctrl *ClientsController) DeleteClientById(c *gin.Context) {
 
 	if err := ctrl.service.DeleteClientById(c.Request.Context(), id); err != nil {
 		ctrl.logger.Error("Failed to delete client", "clientID", id, logger.Err(err), "op", op)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete client"})
+		ctrl.responce(c, http.StatusInternalServerError, gin.H{"error": "Failed to delete client"})
 		return
 	}
 
