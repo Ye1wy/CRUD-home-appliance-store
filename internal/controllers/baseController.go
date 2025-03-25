ef89a093873628b24129b8faa870ff907070e50e
jenconn <ye1wyy@gmail.com>
2025-03-25 17:55:09 +0700

    add: added base controller

---
+++ internal/controllers/baseController.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"CRUD-HOME-APPLIANCE-STORE/internal/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BaseControllerInterface interface {
+}
+
+type BaseController struct {
+	logger *logger.Logger
+}
+
+func NewBaseContorller(logger *logger.Logger) *BaseController {
+	return &BaseController{
+		logger: logger,
+	}
+}
+
+func (ctrl *BaseController) responce(c *gin.Context, statusCode int, obj any) {
+	switch c.GetHeader("Accept") {
+	case "appication/xml":
+		c.XML(statusCode, obj)
+	default:
+		c.JSON(statusCode, obj)
+	}
+}
