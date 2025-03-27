0af7b5cc6fddbe6530c5131ee422073044041a57
jenconn <ye1wyy@gmail.com>
2025-03-27 22:13:52 +0700

    update: updateing clients due common changes

--- internal/services/clientsService.go
+++ internal/services/clientsService.go
@@ -2,78 +2,58 @@ package services
 
 import (
 	"CRUD-HOME-APPLIANCE-STORE/internal/dto"
+	"CRUD-HOME-APPLIANCE-STORE/internal/logger"
 	"CRUD-HOME-APPLIANCE-STORE/internal/mapper"
 	"CRUD-HOME-APPLIANCE-STORE/internal/model"
 	"CRUD-HOME-APPLIANCE-STORE/internal/repositories"
 	"context"
 	"fmt"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type ClientsService interface {
-	AddClient(ctx context.Context, dto *dto.ClientDTO) (*model.Client, error)
-	GetAllClients(ctx context.Context, limit, offset int) ([]dto.ClientDTO, error)
+	CrudServiceInterface[model.Client, dto.ClientDTO]
+	// Create(ctx context.Context, dto dto.ClientDTO) (*model.Client, error)
 	GetClientByNameAndSurname(ctx context.Context, name, surname string) ([]dto.ClientDTO, error)
 	ChangeAddressParameter(ctx context.Context, id string, newAddressId string) error
-	DeleteClientById(ctx context.Context, id string) error
 }
 
 type clientsServiceImpl struct {
-	repo repositories.ClientRepositoryInterface
+	*CrudService[model.Client, dto.ClientDTO]
+	repository repositories.ClientRepositoryInterface
 }
 
-func NewClientService(rep repositories.ClientRepositoryInterface) *clientsServiceImpl {
+func NewClientService(rep repositories.ClientRepositoryInterface, logger *logger.Logger) *clientsServiceImpl {
+	crudService := NewCrudService(rep, mapper.ClientToDTO, mapper.ClientToModel, logger)
 	return &clientsServiceImpl{
-		repo: rep,
+		CrudService: crudService,
+		repository:  rep,
 	}
 }
 
-func (s *clientsServiceImpl) AddClient(ctx context.Context, dto *dto.ClientDTO) (*model.Client, error) {
-	client, err := mapper.ToClientModel(dto)
-	if err != nil {
-		return nil, fmt.Errorf("Client service: Error adding a client: %v", err)
-	}
+// func (s *clientsServiceImpl) Create(ctx context.Context, dto dto.ClientDTO) (*model.Client, error) {
+// 	client, err := mapper.ClientToModel(dto)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Client service: Error adding a client: %v", err)
+// 	}
 
-	client.RegistrationDate = time.Now()
+// 	client.RegistrationDate = time.Now()
 
-	_, err = s.repo.Create(ctx, *client)
-	if err != nil {
-		return nil, fmt.Errorf("Client service: Error adding a client: %v", err)
-	}
+// 	_, err = s.repository.Create(ctx, *client)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Client service: Error adding a client: %v", err)
+// 	}
 
-	// if objectID, ok := result.InsertedID.(primitive.ObjectID); ok {
-	// 	client.Id = objectID.Hex()
-
-	// } else {
-	// 	return nil, fmt.Errorf("failed to parse inserted ID")
-	// }
-
-	return client, nil
-}
-
-func (s *clientsServiceImpl) GetAllClients(ctx context.Context, limit, offset int) ([]dto.ClientDTO, error) {
-	if limit < 0 || offset < 0 {
-		return nil, fmt.Errorf("Client service: limit and offset cannot be less of 0")
-	}
-
-	clients, err := s.repo.GetAll(ctx, limit, offset)
-	if err != nil {
-		return nil, fmt.Errorf("Client service: Error receriving all the client: %v", err)
-	}
-
-	dto := mapper.ToClientDTOs(clients)
-
-	return dto, nil
-}
+// 	return client, nil
+// }
 
 func (s *clientsServiceImpl) GetClientByNameAndSurname(ctx context.Context, name, surname string) ([]dto.ClientDTO, error) {
 	if name == "" || surname == "" {
 		return nil, fmt.Errorf("Service: client name and surname cannot be empty")
 	}
 
-	clients, err := s.repo.GetClientByNameAndSurname(ctx, name, surname)
+	clients, err := s.repository.GetClientByNameAndSurname(ctx, name, surname)
 	if clients == nil && err == nil {
 		return nil, nil
 	}
@@ -82,7 +62,11 @@ func (s *clientsServiceImpl) GetClientByNameAndSurname(ctx context.Context, name
 		return nil, fmt.Errorf("Service: Error when taking a client by first and last name: %v", err)
 	}
 
-	dtos := mapper.ToClientDTOs(clients)
+	dtos := make([]dto.ClientDTO, len(clients))
+
+	for i, item := range clients {
+		dtos[i] = s.mapperD(item)
+	}
 
 	return dtos, nil
 }
@@ -102,19 +86,10 @@ func (s *clientsServiceImpl) ChangeAddressParameter(ctx context.Context, id stri
 		return fmt.Errorf("Service: Error change address parameter: %v", err)
 	}
 
-	err = s.repo.Update(ctx, objectID, objectAddressID)
+	err = s.repository.Update(ctx, objectID, objectAddressID)
 	if err != nil {
 		return fmt.Errorf("Service: Error change address parameter: %v", err)
 	}
 
 	return nil
-}
-
-func (s *clientsServiceImpl) DeleteClientById(ctx context.Context, id string) error {
-	objectId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return fmt.Errorf("Service: Error delete client by id: %v", err)
-	}
-
-	return s.repo.Delete(ctx, objectId)
 }
