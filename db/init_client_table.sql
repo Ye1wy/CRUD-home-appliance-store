4e43370ca4f0514d5d5fb5988e7a500031b399ce
jenconn <ye1wyy@gmail.com>
2025-04-14 16:49:03 +0700

    add: added creation addition table

--- db/init_client_table.sql
+++ db/init_client_table.sql
@@ -9,3 +9,33 @@ CREATE TABLE IF NOT EXISTS client (
     registation_date TIMESTAMP DEFAULT now(),
     address_id UUID NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS image (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    image BYTEA NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS addresses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    country TEXT NOT NULL,
+    city TEXT NOT NULL,
+    street TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price FLOAT NOT NULL,
+    available_stock INT NOT NULL,
+    last_update_date TIMESTAMP DEFAULT now(),
+    supplier_id UUID NOT NULL,
+    image_id UUID NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS supplier (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    address_id UUID NOT NULL,
+    phone_number TEXT NOT NULLs
+);
