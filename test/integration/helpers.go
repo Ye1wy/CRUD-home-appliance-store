4ad23f1c49f64abe88f3767087782c31f244a39b
jenconn <ye1wyy@gmail.com>
2025-06-27 16:24:56 +0700

    update: add new function for extract data from image file

--- test/integration/helpers.go
+++ test/integration/helpers.go
@@ -1,9 +1,15 @@
 package integration
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/jpeg"
+	_ "image/png"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -23,4 +29,47 @@ func createObject(data any, url string) error {
 	}
 
 	return nil
+}
+
+// func createFormData(file, filename, fieldname string) (*multipart.Writer, *bytes.Buffer, error) {
+// 	buf, err := os.Open(file)
+// 	if err != nil {
+// 		return nil, nil, fmt.Errorf("[From sub func \"createFormData\"] Open file error: %w", err)
+// 	}
+
+// 	body := &bytes.Buffer{}
+// 	writer := multipart.NewWriter(body)
+// 	w, err := writer.CreateFormFile(fieldname, filename)
+// 	if err != nil {
+// 		return nil, nil, fmt.Errorf("[From sub func \"createFormData\"] CreateFormFile error: %w", err)
+// 	}
+
+// 	if _, err := io.Copy(w, buf); err != nil {
+// 		return nil, nil, fmt.Errorf("[From sub func \"createFormData\"] Copy error from image buf to: %w", err)
+// 	}
+
+// 	writer.Close()
+
+// 	return writer, body, nil
+// }
+
+func extractImageData(path, filename string) (*bytes.Buffer, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("[From sub func \"extractImageData\"] Open file error: %w", err)
+	}
+	defer file.Close()
+
+	image, _, err := image.Decode(file)
+	if err != nil {
+		return nil, fmt.Errorf("[From sub func \"extractImageData\"] Decode image file error: %w", err)
+	}
+
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, image, nil)
+	if err != nil {
+		return nil, fmt.Errorf("[From sub func \"extractImageData\"] Encode image file error: %w", err)
+	}
+
+	return buf, nil
 }
